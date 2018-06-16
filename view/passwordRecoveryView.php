f83333e81d2eb037eeb6ddc96f410a4ffa465a5f
Christophe Van Maercke <vanmaerckechri@gmail.com>
2018-06-17 00:46:58 +0200

    update css and recovery sms

--- view/passwordRecoveryView.php
+++ view/passwordRecoveryView.php
@@ -1,6 +1,6 @@
 <?php ob_start(); ?>
         <h2>Réinitialisation de votre Mot de Passe</h2>
-        <p>Si vous disposez d'un compte étudiant, veuillez faire une demande auprès de votre administrateur.</p>
+        <p>Si vous disposez d'un compte étudiant, veuillez faire la demande de récupération auprès de votre administrateur.</p>
         <form action="index.php" method="post">
             <label for="email">Adresse e-mail</label>
             <input class="formInput" type="email" name="email" autofocus required>
