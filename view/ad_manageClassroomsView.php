2fc953ca47f33e8f2ad6e1fb886f63ae9fbd8dd1
Christophe Van Maercke <vanmaerckechri@gmail.com>
2018-06-30 01:40:43 +0200

    update rename classroom

--- view/ad_manageClassroomsView.php
+++ view/ad_manageClassroomsView.php
@@ -82,21 +82,24 @@
             let openRenameTool = function(event)
             {
             	let selectedClassrooms = document.querySelector('.list');
-            	// Remove Rename Field if Already Exist
+            	let lastClassroomContainer;
+            	// Return to Normal Last Open Field
+            		// Remove Rename Field if Already Exist
             	if (document.querySelector('.listElementRename'))
             	{
             		let lastElementRename = document.querySelector('.listElementRename');
-            		let lastElementParentRename = lastElementRename.parentElement;
+            		lastElementParentRename = lastElementRename.parentElement;
+            		lastClassroomContainer = lastElementParentRename.parentElement;
             		lastElementParentRename.removeChild(lastElementRename);
             	}
-            	// Remove Submit if Already Exist
+            		// Remove Submit if Already Exist
             	if (document.querySelector('#submit'))
             	{
             		let lastSubmit = document.querySelector('#submit');
             		let lastSubmitParent = lastSubmit.parentElement;
             		lastSubmitParent.removeChild(lastSubmit);
             	}
-            	// Display Name was Hide
+            		// Display Static Name
             	let elementNames = document.querySelectorAll('.list a');
             	for (let i = elementNames.length - 1; i >= 0; i--)
             	{
@@ -107,36 +110,40 @@
             		}
             	}
             	// Create Rename Field
+
             	let buttonRename = event.target;
             	let classroomContainer = buttonRename.parentElement;
-            	let classroomName = classroomContainer.querySelector('a').innerHTML;
-            	let classroomId = classroomContainer.querySelector(".listElementDeleteCheck").value;
-            		// Name Field
-  				let listElementRenameContainer = document.createElement("div"); 
-  				listElementRenameContainer.setAttribute("class", "listElementRenameContainer");
-  				let listElementRename = document.createElement("input"); 
-				listElementRename.setAttribute("type", "text");
-				listElementRename.setAttribute("name", "renameClassroom");
-				listElementRename.setAttribute("value", classroomName);
-				listElementRename.setAttribute("class", "listElementRename");
-				listElementRenameContainer.appendChild(listElementRename);
-            		// classroomId Field
-  				let listElementId = document.createElement("input"); 
-				listElementId.setAttribute("type", "hidden");
-				listElementId.setAttribute("name", "idClassroom");
-				listElementId.setAttribute("value", classroomId);
-				listElementRenameContainer.appendChild(listElementId);
-				classroomContainer.insertBefore(listElementRenameContainer, buttonRename);
-					// submit
-  				let submit = document.createElement("input"); 
-				submit.setAttribute("type", "submit");
-				submit.setAttribute("value", "Enregistrer");
-				submit.setAttribute("id", "submit");
-				classroomContainer.appendChild(submit);
+            	if (lastClassroomContainer != classroomContainer)
+            	{
+	            	let classroomName = classroomContainer.querySelector('a').innerHTML;
+	            	let classroomId = classroomContainer.querySelector(".listElementDeleteCheck").value;
+	            		// Name Field
+	  				let listElementRenameContainer = document.createElement("div"); 
+	  				listElementRenameContainer.setAttribute("class", "listElementRenameContainer");
+	  				let listElementRename = document.createElement("input"); 
+					listElementRename.setAttribute("type", "text");
+					listElementRename.setAttribute("name", "renameClassroom");
+					listElementRename.setAttribute("value", classroomName);
+					listElementRename.setAttribute("class", "listElementRename");
+					listElementRenameContainer.appendChild(listElementRename);
+	            		// classroomId Field
+	  				let listElementId = document.createElement("input"); 
+					listElementId.setAttribute("type", "hidden");
+					listElementId.setAttribute("name", "idClassroom");
+					listElementId.setAttribute("value", classroomId);
+					listElementRenameContainer.appendChild(listElementId);
+					classroomContainer.insertBefore(listElementRenameContainer, buttonRename);
+						// submit
+	  				let submit = document.createElement("input"); 
+					submit.setAttribute("type", "submit");
+					submit.setAttribute("value", "Enregistrer");
+					submit.setAttribute("id", "submit");
+					classroomContainer.appendChild(submit);
 
-				let elementName = classroomContainer.querySelector('.listElementName');
-				elementName.classList.toggle("hide");
-				document.querySelector('.listElementRename').focus();
+					let elementName = classroomContainer.querySelector('.listElementName');
+					elementName.classList.toggle("hide");
+					document.querySelector('.listElementRename').focus();
+				}
             }
             for (let i = button_rename.length - 1; i >= 0; i--)
             {
